29ab7e02d3755dff46f0b18d07e8bce7a3ac703e
l2csi2023 <iscgomal2csi2023@gmail.com>
2023-03-01 20:38:03 +0100

    ajout de deux methode dans ma classe

--- Application_Test_Github/Application_Test_Github/Augustin_Munguiko_Kazungu.cs
+++ Application_Test_Github/Application_Test_Github/Augustin_Munguiko_Kazungu.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;//j'importer cet espqce de nom
+using System.Data;//j'importer cet espqce de nom
 
 namespace Application_Test_Github
 {
     class Augustin_Munguiko_Kazungu
     {
-        
+        private SqlConnection con = new SqlConnection();
+        private SqlCommand cmd = new SqlCommand();
+        //je cree la methode connecter
+        public void connecter(string requetSql)
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.ConnectionString = requetSql;
+                con.Open();
+            }
+        }
+        private void deconnecter()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+
     }
 }
