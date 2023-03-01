24e3d64fc5fb77fddf3f16a29fcdd6b746bbd722
l2csi2023 <iscgomal2csi2023@gmail.com>
2023-03-01 20:18:26 +0100

    Ajout de ma classe pour TP

---
+++ Application_Test_Github/Application_Test_Github/Augustin_Munguiko_Kazungu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Test_Github
+{
+    class Augustin_Munguiko_Kazungu
+    {
+        
+    }
+}
