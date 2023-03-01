0d0f94fc52227e556d9768b3d54f621fee5303d3
l2csi2023 <iscgomal2csi2023@gmail.com>
2023-03-01 16:10:05 +0200

    Ajout évènement

--- Application_Test_Github/Application_Test_Github/Form1.cs
+++ Application_Test_Github/Application_Test_Github/Form1.cs
@@ -16,5 +16,10 @@ namespace Application_Test_Github
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bonjour L2");
+        }
     }
 }
